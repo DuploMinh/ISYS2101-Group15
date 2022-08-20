c1db4eb001ae68a5f46c4f821c85d5c8295ff9ac
phamkiendinh0 <74864852+phamkiendinh0@users.noreply.github.com>
2022-08-20 21:34:36 +0700

    Finish most features for order/delivery page

--- deliverypage/js/delivery.js
+++ deliverypage/js/delivery.js
@@ -1,14 +1,77 @@
 var list = [];
 var totalPrice = 0;
-//This function updates the badge number every time user clicks
-function cartUpdate() {
-    let badge = document.getElementById("badge").innerHTML;
-    badge = Number(badge);
-    badge += 1;
-    document.getElementById("badge").innerHTML = badge;
+
+function subtractCartItem(x) {
+    var target = document.getElementById("amount" + x);
+    if ((target.innerHTML - 1) == 0) {
+        removeCartItem(x);
+        return;
+    }
+
+    for (data of list) {
+        if (data.id == x) {
+            data.amount = target.innerHTML - 1;
+        }
+    }
+
+
+    var price = document.getElementById("price" + x);
+    var single_price = parseFloat(price.innerHTML) / parseInt(target.innerHTML);
+    price.innerHTML = (parseInt(target.innerHTML) - 1) * single_price;
+    target.innerHTML = parseInt(target.innerHTML) - 1;
+    cartUpdate();
+}
+
+function addCartItem(x) {
+    var target = document.getElementById("amount" + x);
+    var price = document.getElementById("price" + x);
+
+    
+    for (data of list) {
+        if (data.id == x) {
+            data.amount = parseInt(target.innerHTML) + 1;
+        }
+    }
+
+    var single_price = parseFloat(price.innerHTML) / parseInt(target.innerHTML);
+    price.innerHTML = (parseInt(target.innerHTML) + 1) * single_price;
+    target.innerHTML = parseInt(target.innerHTML) + 1;
+    cartUpdate();
+}
+
+function removeCartItem(x) {
+    var target = document.getElementById("c-item" + x);
+    console.log(list);
+    var temp = [];
+    for (data of list) { 
+        if (data.id == x) {
+            continue;
+        }
+        else {
+            temp.push(data);
+        }
+    }
+    list = temp;
+    console.log(list);
+    target.remove();
+    if (list.length == 0) {
+        var element = document.getElementById("totalPrice");
+        element.remove();
+    }
+    cartUpdate();
 }
 
 
+//This function updates the badge number every time user clicks
+function cartUpdate() {
+    document.getElementById("badge").innerHTML = list.length;
+}
+
+
+function checkOut() {
+    sessionStorage.setItem('list', JSON.stringify(list));
+    window.open("../../cart/cart.html");
+}
 
 function addItem(x) {
 
@@ -54,7 +117,7 @@ function adder(data, x) {
                 totalPrice += price;
                 list.push(json);
                 document.getElementById("cart-list").innerHTML += 
-                '<li class="list-group-item cart-item d-flex mt-2 mb-2">'+
+                '<li class="list-group-item cart-item d-flex mt-2 mb-2" id="c-item' + key.id + '">'+
                 '<div class="d-block">'+
                     '<div class="cart-item-name justify-content-center text-center p-2 mb-2">'+
                         '<h4 class="h4">' + key.name + '</h4>' +
@@ -66,7 +129,7 @@ function adder(data, x) {
                         '<div class="w-50 d-block m-auto gap-4 justify-content-center cart-item-button" id="cart-item-button">'+
                             '<div class="d-flex gap-4 h-50 justify-content-center">'+
                                 '<div class="subtract-item">'+
-                                    '<button type="button" class="btn btn-dark btn-sm item-button overflow-hidden" id="subtract">'+
+                                    '<button type="button" class="btn btn-dark btn-sm item-button overflow-hidden" id="subtract' + key.id + '" onclick="subtractCartItem(' + key.id+ ')">'+
                                         '<i class="bi bi-dash-lg overflow-auto">'+'</i>'+
                                     '</button>'+
                                 '</div>'+
@@ -76,7 +139,7 @@ function adder(data, x) {
                                 '</div>'+
       
                                 '<div class="add-item">'+
-                                    '<button type="button" class="btn btn-dark btn-sm item-button" id="add">'+ 
+                                    '<button type="button" class="btn btn-dark btn-sm item-button" id="add' + key.id + '" onclick="addCartItem(' + key.id + ')">'+ 
                                         '<i class="bi bi-plus-lg"></i>'+
                                     '</button>'+
                                 '</div>'+
@@ -94,30 +157,34 @@ function adder(data, x) {
             }
         }
     }
+
     if (document.getElementById("totalPrice") !== null) {
         document.getElementById("totalPrice").remove();
         document.getElementById("cart-list").innerHTML += 
         '<li class="list-group-item cart-item d-flex mt-2 mb-2 bg-warning" id="totalPrice">'+
             '</div>'+
-            '<div class="d-flex h-50 justify-content-center mt-3 p-auto">'+
+            '<div class="d-flex h-50 justify-content-between mt-3 p-auto">'+
             '<h4 class="h4">'+ 'Total Price: ' + '</h4>'+
             '<h4 class="h4" id="total-item-price">' + totalPrice + '</h4>'+
             '<h4 class="h4"> $ </h4>' +
+            '<button class="btn btn-primary" onclick="checkOut();">Check Out</button>'
             '</div>'+
             '</div>'+ 
-            '</li>';
+        '</li>';
         // document.getElementById("total-item-price").innerHTML = totalPrice;
     }
     else {
         document.getElementById("cart-list").innerHTML += 
         '<li class="list-group-item cart-item d-flex mt-2 mb-2 bg-warning" id="totalPrice">'+
             '</div>'+
-            '<div class="d-flex h-50 justify-content-center mt-3 p-auto position-relative">'+
+            '<div class="d-flex h-50 justify-content-between mt-3 p-auto">'+
             '<h4 class="h4">'+ 'Total Price: ' + '</h4>'+
             '<h4 class="h4" id="total-item-price">' + totalPrice + '</h4>'+
             '<h4 class="h4"> $ </h4>' +
+            '<button class="btn btn-primary" onclick="checkOut();">Check Out</button>' +
             '</div>'+
             '</div>'+ 
-            '</li>';
+        '</li>';
     }
+    cartUpdate();
 }
