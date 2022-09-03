716eed9392389fccddab4efdb496ca257bad0332
doquangthang-zet <s3891873@rmit.edu.vn>
2022-09-03 22:15:25 +0700

    start connect to api

---
+++ src/main/resources/static/js/cart.js
@@ -0,0 +1,134 @@
+//Retrieve From Order Page
+var list = []
+var jsonList = JSON.parse(sessionStorage.getItem('list'));
+
+
+start();
+function start() {
+    if (jsonList.length != 0 && list.length == 0) {
+        list = jsonList;
+        addItem()
+    }
+    else {
+        addItem();
+    }
+}
+
+function decrease(x) {
+    let amount = document.getElementById("amount" + x);
+    if ((amount.innerHTML - 1) == 0) {
+        removeItem(x);
+        return;
+    }
+    let target = document.getElementById("amount" + x);
+    let price = document.getElementById("price" + x);
+    for (data of list) {
+        if (data.id == x) {
+            data.amount = parseInt(target.innerHTML) + 1;
+        }
+    }
+    let quantity = target.innerHTML;
+    let pos = price.innerHTML.indexOf(':');
+    let subString = price.innerHTML.substring(pos + 2, price.innerHTML.length - 1);
+
+
+    let single_price = parseFloat(subString) / parseInt(target.innerHTML);
+    // price.innerHTML = "Price: " + toString(parseInt(target.innerHTML + 1) * parseFloat(single_price));
+    // target.innerHTML = parseInt(target.innerHTML + 1);
+    
+    let total = document.getElementById("total-money").innerHTML;
+    total = total.substring(total.indexOf(':') +2, total.length - 1);
+    
+    target.innerHTML = parseInt(quantity) - 1;
+    price.innerHTML = "Price: " + (parseInt(target.innerHTML) * single_price) + "$";
+    document.getElementById("total-money").innerHTML = "Total Price: " + (parseFloat(total) - single_price) + "$";
+}
+
+function increase(x) {
+    let target = document.getElementById("amount" + x);
+    let price = document.getElementById("price" + x);
+    for (data of list) {
+        if (data.id == x) {
+            data.amount = parseInt(target.innerHTML) + 1;
+        }
+    }
+    let quantity = target.innerHTML;
+    let pos = price.innerHTML.indexOf(':');
+    let subString = price.innerHTML.substring(pos + 2, price.innerHTML.length - 1);
+
+
+    let single_price = parseFloat(subString) / parseInt(target.innerHTML);
+    // price.innerHTML = "Price: " + toString(parseInt(target.innerHTML + 1) * parseFloat(single_price));
+    // target.innerHTML = parseInt(target.innerHTML + 1);
+    
+    let total = document.getElementById("total-money").innerHTML;
+    total = total.substring(total.indexOf(':') +2, total.length - 1);
+    
+    target.innerHTML = parseInt(quantity) + 1;
+    price.innerHTML = "Price: " + (parseInt(target.innerHTML) * single_price) + "$";
+    document.getElementById("total-money").innerHTML = "Total Price: " + (parseFloat(total) + single_price) + "$";
+}
+
+function removeItem(x) {
+    var target = document.getElementById("item" + x);
+    var temp = [];
+    for (data of list) { 
+        if (data.id == x) {
+            continue;
+        }
+        else {
+            temp.push(data);
+        }
+    }
+    list = temp;
+    target.remove();
+
+    let total = 0;
+    var target = document.getElementById("cart");
+    for (item of list) {
+        total += item.price * item.amount;
+    }
+    document.getElementById("total-money").innerHTML = "Total Price: " + total + "$";
+}
+
+function addItem() {
+
+    fetch("../static/js/menu.json")
+        .then(response => response.json())
+        .then(data => {
+            adder(data);
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+  }
+
+function adder(data) {
+    var target = document.getElementById("cart");
+    let total = 0;
+    for (key of data) {
+        for (item of list) {
+            if (key.id == item.id) {
+                target.innerHTML +=
+                '<div class="shoppingCart__item row bg-light position-relative rounded-3 m-2 p-2 shadow" id="item' + key.id + '">' + 
+                '<h4>Description: </h4>' +
+                '<h6>' + key.description + '</h6>' + 
+                '<img class="img-fluid col-md-3 rounded-3" src="./'+ key.image + '">' + 
+                '<div class="col">' + 
+                '<h4>' + key.name + '</h4>' +
+                '<p>Tax: 10%</p>' +
+                '<div class="adding__icon d-flex align-items-center">' +
+                    '<i class="btn fa-regular fa-square-minus" onclick="decrease(' + key.id + ');"></i>' + 
+                        '<span id="amount' + key.id + '">' + item.amount + '</span>' + 
+                    '<i class="btn fa-regular fa-square-plus" onclick="increase(' + key.id + ');"></i>' +
+                    '</div>' + 
+                '</div>' +
+                    '<i class="fa-regular fa-circle-xmark" id="close_btn" onclick="removeItem(' + key.id + ');"></i>' + 
+                '<p class="price__item position-absolute bottom-0 w-50 text-end end-0 fw-bold" id="price' + key.id + '">' + 'Price: ' + (item.price * item.amount) + '$</p>' + 
+                '</div>';
+                total += item.price * item.amount;
+            }
+        }
+    }
+    document.getElementById("total-money").innerHTML = "Total Price: " + total + "$";
+}
---
+++ src/main/resources/static/js/menu.js
@@ -0,0 +1,150 @@
+var list = [];
+
+//UPLOAD IMAGE FORM
+const post_btn = document.getElementById("post_address_btn");
+const overlay = document.getElementById("overlay");
+const close_btn = document.querySelector("#close_btn");
+
+post_btn.onclick = function ()  {
+    overlay.style.display = "grid";
+}
+
+close_btn.onclick = function() {
+    overlay.style.display = "none";
+}
+
+overlay.addEventListener('click', function(e) {
+    if (e.target.id == 'overlay') {
+        overlay.style.display = "none";
+    }
+})
+
+
+//Map feature
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -33.8688, lng: 151.2195},
+      zoom: 13
+    });
+    var input = document.getElementById('search_input');
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
+
+    var infowindow = new google.maps.InfoWindow();
+    var marker = new google.maps.Marker({
+        map: map,
+        anchorPoint: new google.maps.Point(0, -29)
+    });
+
+    autocomplete.addListener('place_changed', function() {
+        infowindow.close();
+        marker.setVisible(false);
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            window.alert("Autocomplete's returned place contains no geometry");
+            return;
+        }
+  
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+        }
+        marker.setIcon(({
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(35, 35)
+        }));
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+    
+        var address = '';
+        if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+        }
+    
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+        infowindow.open(map, marker);
+    });
+}
+
+function goToVoucher() {
+    window.location = "./voucher.html";
+    window.location.href = "./voucher.html";
+    window.location.assign("./voucher.html");
+}
+
+
+function addItem(x) {
+
+    fetch("../static/js/menu.json")
+        .then(response => response.json())
+        .then(data => {
+            adder(data,x);
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+  }
+
+
+  function adder(data, x) {
+    var exist = false;
+    var target = document.getElementById("cart-quantity");
+    target.innerHTML = Number(target.innerHTML) + 1;
+    document.getElementById("badge").innerHTML = Number(document.getElementById("badge").innerHTML) + 1;
+    if (list.length != 0) {
+        list.forEach(element => {
+            if (element.id == x) {
+                exist = true;
+                let amount_id = "amount" + element.id;
+                let price_id = "price" + element.id;
+                let amount = element.amount + 1;
+                let price = element.price * amount;
+                element.amount += 1;
+            }
+        });
+    }
+
+    for (key of data) {
+        if(key.id == x) {
+            if (!exist) {
+                let id = key.id;
+                let amount = Number(1);
+                let price = Number(key.price);
+                let json = {
+                    "id":id,
+                    "amount": amount, 
+                    "price": price
+                }
+                list.push(json);
+                break;
+            }
+        }
+    }
+    sessionStorage.setItem('list', JSON.stringify(list));
+    cartUpdate();
+}  
+
+
+function cartUpdate() {
+    if(list.length !== 0) {
+        document.getElementById("badge").innerHTML = list.length;
+        document.getElementById("badge2").innerHTML = list.length;
+        document.getElementById("badge").style.display = "grid";
+        document.getElementById("badge2").style.display = "grid";
+    } else {
+        document.getElementById("badge").style.display = "none";
+        document.getElementById("badge2").style.display = "none";
+    }
+}
