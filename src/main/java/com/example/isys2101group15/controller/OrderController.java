4445ca53c3578b5fc27674805f118b0445344c01
Minh Vu <minh.duplo@gmail.com>
2022-09-11 18:31:22 +0700

    Updated endpoints

--- src/main/java/com/example/isys2101group15/controller/OrderController.java
+++ src/main/java/com/example/isys2101group15/controller/OrderController.java
@@ -1,9 +1,15 @@
 package com.example.isys2101group15.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/order")
 public class OrderController {
-
+  @GetMapping("/cart")
+  public String cart(){
+    return "cart";
+  }
 }
